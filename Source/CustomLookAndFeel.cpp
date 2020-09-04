64a05983c6efd634dde9291d3f95bb300429ba35
Christian Ahrens <christianahrens@me.com>
2020-09-04 10:51:04 +0200

    Improved button background colouring for On vs. Off state

--- Source/CustomLookAndFeel.cpp
+++ Source/CustomLookAndFeel.cpp
@@ -38,12 +38,14 @@ CustomLookAndFeel::CustomLookAndFeel()
     setColour(PopupMenu::ColourIds::highlightedBackgroundColourId, Colours::white);
     setColour(PopupMenu::ColourIds::highlightedTextColourId, Colours::lightgrey);
 
-	setColour(TextButton::ColourIds::buttonColourId, Colours::darkgrey.brighter());
-	setColour(TextButton::ColourIds::buttonOnColourId, Colours::lightgrey);
+	setColour(TextButton::ColourIds::buttonColourId, Colours::darkgrey); // this applies for DrawableButton as well
+	setColour(TextButton::ColourIds::buttonOnColourId, Colours::grey); // this applies for DrawableButton as well
 	setColour(TextButton::ColourIds::textColourOffId, Colours::white);
 	setColour(TextButton::ColourIds::textColourOnId, Colours::white);
 
-	setColour(DrawableButton::ColourIds::backgroundColourId, Colours::darkgrey);
+    setColour(DrawableButton::ColourIds::textColourId, Colours::white);
+    setColour(DrawableButton::ColourIds::textColourOnId, Colours::white);
+    setColour(DrawableButton::ColourIds::backgroundColourId, Colours::darkgrey);
 	setColour(DrawableButton::ColourIds::backgroundOnColourId, Colours::darkgrey);
     
     setColour(ListBox::ColourIds::backgroundColourId, Colours::darkgrey);
