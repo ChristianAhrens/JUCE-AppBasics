6f74771ecd82acf3583ebd43cbad23103e61bc39
Christian Ahrens <christianahrens@me.com>
2023-10-12 17:39:16 +0200

    Added FixedFontTextEditor for texteditors that change their font according to LookAndFeel changes

---
+++ Source/FixedFontTextEditor.cpp
@@ -0,0 +1,54 @@
+/* Copyright(c) 2023, Christian Ahrens
+ *
+ * This file is part of JUCE-AppBasics <https://github.com/ChristianAhrens/JUCE-AppBasics>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#include "FixedFontTextEditor.h"
+
+
+namespace JUCEAppBasics
+{
+
+
+/**
+ * Constructor that simply acts as proxy to TextEditor base class constructor
+ */
+FixedFontTextEditor::FixedFontTextEditor(const juce::String& componentName, juce_wchar passwordCharacter)
+	: juce::TextEditor(componentName, passwordCharacter)
+{
+}
+
+/**
+ * Destructor
+ */
+FixedFontTextEditor::~FixedFontTextEditor()
+{
+
+}
+
+/**
+ * Reimplemented method that extends the base class implementation
+ * with setting the text colour to default of changed lookAndFeel
+ */
+void FixedFontTextEditor::lookAndFeelChanged()
+{
+	applyColourToAllText(getLookAndFeel().findColour(TextEditor::textColourId), true);
+
+	juce::TextEditor::lookAndFeelChanged();
+}
+
+
+}
---
+++ Source/FixedFontTextEditor.h
@@ -0,0 +1,44 @@
+/* Copyright(c) 2023, Christian Ahrens
+ *
+ * This file is part of JUCE-AppBasics <https://github.com/ChristianAhrens/JUCE-AppBasics>
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License version 3.0 as published
+ * by the Free Software Foundation.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE.  See the GNU Lesser General Public License for more
+ * details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this library; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+ */
+
+#pragma once
+
+#include <JuceHeader.h>
+
+namespace JUCEAppBasics
+{
+
+//==============================================================================
+/**
+ * FixedFontTextEditor is a reimplementation of a juce:TextEditor with the only
+ * differing behaviour to reset all text colour to what is set as default in
+ * current juce::Desktop lookAndFeel on every lookAndFeelChanged call.
+ */
+class FixedFontTextEditor : public juce::TextEditor
+{
+public:    
+    explicit FixedFontTextEditor(const juce::String& componentName = String(),
+        juce_wchar passwordCharacter = 0);
+    virtual ~FixedFontTextEditor() override;
+
+    //==============================================================================
+    void lookAndFeelChanged() override;
+
+};
+
+}
